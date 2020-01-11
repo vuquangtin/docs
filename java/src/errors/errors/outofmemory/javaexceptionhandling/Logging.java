ccb750ed9ee0a6bcf88eadd11cb65289aafb28f4
tinvq <tinvuquang@admicro.vn>
2020-01-11 15:55:01 +0700

    commit code

---
+++ java/src/errors/errors/outofmemory/javaexceptionhandling/Logging.java
@@ -0,0 +1,184 @@
+package errors.outofmemory.javaexceptionhandling;
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.ClassUtils;
+import org.apache.commons.lang3.builder.*;
+
+/**
+ * 
+ * 
+ * @author EMAIL:vuquangtin@gmail.com , tel:0377443333
+ * @version 1.0.0
+ * @see <a
+ *      href="https://github.com/vuquangtin/docs">https://github.com/vuquangtin/docs</a>
+ *
+ */
+public class Logging {
+	private static final char separatorCharacterDefault = '-';
+	private static final String separatorInsertDefault = "";
+	private static final int separatorLengthDefault = 40;
+
+	/**
+	 * Get a String of passed char of passed length size.
+	 * 
+	 * @param character
+	 *            Character to repeat.
+	 * @param length
+	 *            Length of string.
+	 * @return Created string.
+	 */
+	private static String getRepeatedCharString(char character, int length) {
+		// Create new character array of proper length.
+		char[] characters = new char[length];
+		// Fill each array element with character.
+		Arrays.fill(characters, character);
+		// Return generated string.
+		return new String(characters);
+	}
+
+	/**
+	 * Outputs any kind of Object. Uses ReflectionToStringBuilder from Apache
+	 * commons-lang library.
+	 *
+	 * @param value
+	 *            Object to be output.
+	 */
+	public static void log(Object value) {
+		if (value == null)
+			return;
+		// If primitive or wrapper object, directly output.
+		if (ClassUtils.isPrimitiveOrWrapper(value.getClass())) {
+			System.out.println(value);
+		} else {
+			// For complex objects, use reflection builder output.
+			System.out.println(new ReflectionToStringBuilder(value,
+					ToStringStyle.MULTI_LINE_STYLE).toString());
+		}
+	}
+
+	/**
+	 * Outputs any kind of String.
+	 *
+	 * @param value
+	 *            String to be output.
+	 */
+	public static void log(String value) {
+		if (value == null)
+			return;
+		System.out.println(value);
+	}
+
+	/**
+	 * Outputs passed in Throwable exception or error instance. Can be
+	 * overloaded if expected parameter should be specified.
+	 *
+	 * @param throwable
+	 *            Throwable instance to output.
+	 */
+	public static void log(Throwable throwable) {
+		// Invoke call with default expected value.
+		log(throwable, true);
+	}
+
+	/**
+	 * Outputs passed in Throwable exception or error instance. Includes
+	 * Throwable class type, message, stack trace, and expectation status.
+	 *
+	 * @param throwable
+	 *            Throwable instance to output.
+	 * @param expected
+	 *            Determines if this Throwable was expected or not.
+	 */
+	public static void log(Throwable throwable, boolean expected) {
+		System.out.println(String.format("[%s] %s", expected ? "EXPECTED"
+				: "UNEXPECTED", throwable.toString()));
+		throwable.printStackTrace();
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator() {
+		lineSeparator(separatorInsertDefault, separatorLengthDefault,
+				separatorCharacterDefault);
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator(String insert) {
+		lineSeparator(insert, separatorLengthDefault, separatorCharacterDefault);
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator(int length) {
+		lineSeparator(separatorInsertDefault, length, separatorCharacterDefault);
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator(int length, char separator) {
+		lineSeparator(separatorInsertDefault, length, separator);
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator(char separator) {
+		lineSeparator(separatorInsertDefault, separatorLengthDefault, separator);
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator(String insert, int length) {
+		lineSeparator(insert, length, separatorCharacterDefault);
+	}
+
+	/**
+	 * See: lineSeparator(String, int, char)
+	 */
+	public static void lineSeparator(String insert, char separator) {
+		lineSeparator(insert, separatorLengthDefault, separator);
+	}
+
+	/**
+	 * Outputs a dashed line separator with inserted text centered in the
+	 * middle.
+	 *
+	 * @param insert
+	 *            Inserted text to be centered.
+	 * @param length
+	 *            Length of line to be output.
+	 * @param separator
+	 *            Separator character.
+	 */
+	public static void lineSeparator(String insert, int length, char separator) {
+		// Default output to insert.
+		String output = insert;
+
+		if (insert.length() == 0) {
+			output = getRepeatedCharString(separator, length);
+		} else if (insert.length() < length) {
+			// Update length based on insert length, less a space for margin.
+			length -= (insert.length() + 2);
+			// Halve the length and floor left side.
+			int left = (int) Math.floor(length / 2);
+			int right = left;
+			// If odd number, add dropped remainder to right side.
+			if (length % 2 != 0)
+				right += 1;
+
+			// Surround insert with separators.
+			output = String.format("%s %s %s",
+					getRepeatedCharString(separator, left), insert,
+					getRepeatedCharString(separator, right));
+		}
+
+		System.out.println(output);
+	}
+}
